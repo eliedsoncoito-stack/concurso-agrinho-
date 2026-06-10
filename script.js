6535fdd9337d9eaa59ae12ee27a3711095007186
eliedsoncoito-stack <eliedson.coito@escola.pr.gov.br>
2026-06-10 09:18:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+// Script interativo do Agrinho 2026
 
+function inscrever() {
+    alert("🎉 Inscrições para o Agrinho 2026 abertas!\n\nEm breve entraremos em contato com sua escola. 🌱");
+}
+
+// Efeito de scroll suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Efeito de brilho no título ao carregar
+window.addEventListener('load', () => {
+    console.log("%cAgrinho 2026 - Paraná carregado com sucesso! 🚜", "color: #FF00FF; font-size: 16px;");
+});
