e423b804adcb9495c78d36defcf6b066e355fbf4
eliedsoncoito-stack <eliedson.coito@escola.pr.gov.br>
2026-08-05 08:44:53 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let likes = [0, 0];
+let likes = [0, 0, 0];
 
 function curtir(post) {
     likes[post]++;
-    document.getElementById("likes" + post).innerText = likes[post];
+    document.getElementById("likes" + post).textContent = likes[post];
 }
